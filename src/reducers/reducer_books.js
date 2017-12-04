d35346bd31648d04d356bdb2b2b8e61e708af050
Derek <derektaylor.us@gmail.com>
2017-12-04 13:19:51 -0600

    Initial commit + book list container lessons

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+export default function () {
+    return [
+        {
+            title: 'JS: The Good Parts'
+        }, {
+            title: 'Harry Potter'
+        }, {
+            title: 'The Dark Tower'
+        }, {
+            title: 'Eloquent Ruby'
+        }
+    ];
+}
